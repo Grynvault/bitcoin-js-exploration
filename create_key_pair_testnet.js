e14a48d3d973f2c5b6441794583d98b628d1c73e
Izzul <izzulsyahmi210@gmail.com>
2025-03-04 14:26:13 +0800

    added CLTV on testnet

---
+++ create_key_pair_testnet.js
@@ -0,0 +1,21 @@
+/** @format */
+
+import { ECPairFactory } from 'ecpair';
+import * as ecc from 'tiny-secp256k1';
+import * as bitcoin from 'bitcoinjs-lib';
+import { randomBytes } from 'crypto';
+
+const TESTNET = bitcoin.networks.testnet;
+
+const rng = () => randomBytes(32);
+
+const ECPair = ECPairFactory(ecc);
+
+const keyPair = ECPair.makeRandom({ network: TESTNET, rng });
+//Save this private key
+// Output: <Buffer 93 5b 1d 0e da 99 00 03 a8 92 68 32 40 06 cd a9 bd 51 47 a3 74 50 ea 02 18 13 44 ef 3b d3 9d 3d>
+// Save as 935b1d0eda990003a89268324006cda9bd5147a37450ea02181344ef3bd39d3d
+console.log('Save the privateKey ->', keyPair.privateKey);
+
+const { address } = bitcoin.payments.p2wpkh({ pubkey: keyPair.publicKey, network: TESTNET });
+console.log('Save the address -> ', address); //This is the address that will be used to send/receive funds
